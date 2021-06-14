Mistake on computation of power nomalization in sim_power.sbend_pl corrected

kronos/utils/sim_power.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
+from scipy.fft import rfftfreq
 
-def sbend_pl(f,a1,a2,b=1,f0=1,norm=None,dc=10,frac_rms=0.3,df=1):
+def sbend_pl(dt,nt,freq_array=None,a1=0,a2=-1,b=10,f0=1,norm=None,dc=10,frac_rms=0.3):
     '''
     It returns a (smoothly) bending power law power spectral shape
     given a certain total fractional RMS amplitude and a normalizaion
@@ -9,8 +10,15 @@ def sbend_pl(f,a1,a2,b=1,f0=1,norm=None,dc=10,frac_rms=0.3,df=1):
 
     PARAMETERS
     ----------
-    f: sequence or float
-        Frequency array 
+    dt: float
+        Time frequency
+    nt: int
+        Number of time bins
+    freq_array: sequence, float, or None
+        Frequency values where to evaluate the (normalized) power.
+        If None (default), an array of positive frequencies will
+        be evaluated according to the provided time resolution and
+        number of time bins
     a1: float
         Power law index at low frequencies
     a2: float
@@ -28,15 +36,10 @@ def sbend_pl(f,a1,a2,b=1,f0=1,norm=None,dc=10,frac_rms=0.3,df=1):
         Other options are:
         - POWER
         - LEAHY
-        - RMS
-        
+        - RMS        
     frac_rms: float
         Total fractional RMS
-    df: float
-        Frequency resolution. This is used only in the case the 
-        frequency array is longer than 2. It is supposed to be 
-        specified when you want to evaluate the power at a single
-        frequency
+
 
     RETURNS
     -------
@@ -46,34 +49,40 @@ def sbend_pl(f,a1,a2,b=1,f0=1,norm=None,dc=10,frac_rms=0.3,df=1):
     HISTORY
     -------
     2021 06 01, Stefano Rapisarda (Uppsala), Creation date
+    2021 06 14, Stefano Rapisarda (Uppsala)
+        Mistake about RMS comutation corrected. I did not take into 
+        account that the initially provided frequency array could be
+        not linearly spaced. I added two parameters (dt,nt), so that
+        the script provide a valid frequency array for computing RMS
+        normalization
     '''
 
-    if not type(f) in [list,np.ndarray,pd.Series]:
-        f = np.array([f])
-    elif type(f) == list:
-        f = np.array(f)
-
-    x = f/f0
-    result = x**a1/( (1.0+x**b)**((-a2+a1)/b) )
+    f = rfftfreq(n=nt,d=dt)
+    df = f[2]-f[1]
     
-    if len(f) > 2:
-        df = f[2]-f[1]
-
-    # Get rid of the zero frequency component
-    start=0
-    if f[0] == 0.: start=1
-        
-    # Applying normalization according to specified option
     if not norm is None:
+        
+        f = rfftfreq(n=nt,d=dt)
+        xtmp = f/f0
+        power = xtmp**a1/( (1.0+xtmp**b)**((-a2+a1)/b))
+
         if norm.upper() == 'POWER':
             # There is not 2 because the None normalization takes
             # into account both positive and negative power
-            current_frac_rms2 = np.sum(result[start:])/dc**2
+            current_frac_rms2 = np.sum(power)/dc**2
         elif norm.upper() == 'LEAHY':
-            current_frac_rms2 = np.sum(result[start:])/dc
+            current_frac_rms2 = np.sum(power)/dc
         elif norm.upper() == 'RMS':
-            current_frac_rms2 = np.sum(result[start:])*df
+            current_frac_rms2 = np.sum(power)*df
 
-        result = result/current_frac_rms2*frac_rms**2
+        norm_factor = frac_rms**2/current_frac_rms2
+                       
+    if freq_array is None:
+        x = f/f0
+    else:
+        if type(freq_array) == list: freq_array = np.array(freq_array)  
+        x = freq_array/f0
+                       
+    result = norm_factor * x**a1/( (1.0+x**b)**((-a2+a1)/b))
     
     return result

kronos/utils/time_series.py
@@ -619,7 +619,7 @@ def timmer_koenig_from_ps(dt,nt,ps,dc=0,frac_rms = 0.1):
     # The second 1/2 is vecause the power is equally distributed
     # between positive and negative frequencies (and here we 
     # are considering only the negative frequencies)
-    pos_amp = np.sqrt(0.25*ps)*(n1+n2*1j)
+    pos_amp = np.sqrt(0.5*ps)*(n1+n2*1j)
 
     # If nt is even, the Nyquist frequency is included in the
     # Fourier amplitudes. It is real and unique for both negative

kronos/wavelets/wavelets.py
@@ -6,6 +6,10 @@ import pathlib
 import math
 import pickle
 
+import functools
+
+import concurrent
+
 from scipy.stats import chi2
 from scipy.signal import convolve, wavelets
 from scipy.fftpack import fft, fftfreq
@@ -408,10 +412,9 @@ class WaveletTransform:
         if len(self.wt) == 0: return None
         return (abs(self.wt))**2
 
-    @property
-    def global_power(self):
+    def global_power(self,norm_type=''):
         if len(self.wt) == 0: return None
-        return self.norm_power().mean(axis=1)
+        return self.norm_power(norm_type).mean(axis=1)
 
     @property
     def family(self):
@@ -421,7 +424,7 @@ class WaveletTransform:
         if len(self.time) == 0 or len(self.wt) == 0: return None
         result = self.power/np.var(self.counts)
         if norm_type.upper() == 'LEAHY':
-            result /= np.sum(self.counts)
+            result = result/np.sum(self.counts)
         elif norm_type.upper() == 'RMS':
             result = result*len(self.time)*self.tres/np.sum(self.counts)**2
         return result
@@ -664,7 +667,8 @@ class WaveletTransform:
 class Patches:
 
     def __init__(self,patches=None,nf=None,nt=None,
-        verdicts=None,flags=None,meta_data=None,notes=None):
+        verdicts=None,flags=None,conf_level=None,
+        meta_data=None,notes=None):
 
         self.patches = patches
         self.nf = nf
@@ -672,12 +676,15 @@ class Patches:
         self.verdicts = verdicts
         self.flags = flags
 
+        self.conf_level = conf_level
+
         self.notes = notes
 
         if meta_data is None:
             self.meta_data = {}
         else: self.meta_data = meta_data
         self.meta_data['PATCHES_CRE_DATE'] = my_cdate()
+
         
     @staticmethod
     def extract_patches(wavelet, bkg_power=1., conf_level = 0.9975):
@@ -734,7 +741,7 @@ class Patches:
         # -------------------------------------------------------------
 
         result = Patches(patches=patches,nf=nf,nt=nt,verdicts=None,
-        meta_data=None,notes=None)
+        conf_level=conf_level,meta_data=None,notes=None)
         result.meta_data['PATCHES_CRE_MODE'] = 'Patches extracted from wavelet object'
         result.meta_data['CONF_LEVEL'] = conf_level
 
@@ -818,8 +825,7 @@ class Patches:
 
         if ax is None: return fig,ax
 
-    def evaluate_patch_single(self,wavelet,bkg_power,patch_i,
-        conf_level,tollerance):
+    def evaluate_patch_single(self,patch_i,wavelet,bkg_power,tollerance):
 
         if len(wavelet.freqs) != len(bkg_power):
             raise ValueError('The backgound power must have the same wavelet.freqs dimension')
@@ -850,7 +856,7 @@ class Patches:
         # Averaging power over time
         average_power = np.sum(masked_power,axis=1)/n_t_bins
             
-        norm = np.flip(bkg_power)[y_mask]*chi2.ppf(conf_level,df=dof)/dof
+        norm = np.flip(bkg_power)[y_mask]*chi2.ppf(self.conf_level,df=dof)/dof
         norm_power = average_power/norm
         
         #print(norm_power)
@@ -869,22 +875,35 @@ class Patches:
             
         return verdict,patch_flags
 
-    def evaluate_patches(self,wavelet,bkg_power,conf_level=None,tollerance=0.95,show_progress=False):
+    def evaluate_patches(self,wavelet,bkg_power,tollerance=0.95,show_progress=False,
+        multi_process=False,print_info=True):
 
-        if conf_level is None: conf_level = self.meta_data['CONF_LEVEL']
         self.meta_data['TOLLERANCE'] = 0.95
 
-        verdicts = []
-        flags = []
-        for p in range(len(self.patches)):
-            if show_progress: print('Processing patch {}/{}'.format(p+1,len(self.patches)))
-            result = self.evaluate_patch_single(wavelet,bkg_power=bkg_power,\
-                patch_i=p,conf_level=conf_level,tollerance=tollerance)
-            verdicts += [result[0]]
-            flags += [result[1]]
+        if multi_process:
+            partial_evaluate = functools.partial(self.evaluate_patch_single,wavelet = wavelet,\
+                bkg_power=bkg_power,tollerance=tollerance)
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                results = list(executor.map(partial_evaluate,[p for p in range(len(self.patches))]))
+            self.verdicts = [r[0] for r in results]
+            self.flags = [r[1] for r in results]
 
-        self.verdicts = verdicts
-        self.flags = flags
+        else:
+            verdicts = []
+            flags = []
+
+            for p in range(len(self.patches)):
+                if show_progress: print('Processing patch {}/{}'.format(p+1,len(self.patches)))
+                result = self.evaluate_patch_single(patch_i=p,wavelet=wavelet,bkg_power=bkg_power,\
+                tollerance=tollerance)
+                verdicts += [result[0]]
+                flags += [result[1]]
+
+            self.verdicts = verdicts
+            self.flags = flags
+
+        print('Patches Evaluated')
+
 
     def save(self,file_name='wavelet_patches.pkl',fold=pathlib.Path.cwd()):
 